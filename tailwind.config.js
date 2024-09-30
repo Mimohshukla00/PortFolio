3d145e982e503701a88a7637e0af0a2e913e2c73
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-10-01 04:08:13 +0530

    new import

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,20 @@
+const { nextui } = require("@nextui-org/theme");
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/components/(button|link|ripple|spinner).js"
+  ],
   theme: {
     extend: {
       colors: {
         customDark: "#080C0C",
         teal: "#0E6669",
         customBlue: "rgb(14 165 233)",
+        dark: "#1E1E1E",
       },
     },
   },
-  plugins: [],
+  plugins: [nextui()],
 };
