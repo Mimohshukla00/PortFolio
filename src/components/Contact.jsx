748ac00c70b3d24a9ddec0d33ef9c4296f1542ae
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-10-02 03:20:49 +0530

    contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,44 +7,16 @@ export default function Contact() {
         <div className="mt-8 overflow-hidden rounded-lg shadow-lg bg-white">
           <div className="grid grid-cols-1 md:grid-cols-2">
             {/* Left Contact Information Section */}
-            <div className="p-6 bg-gray-100 sm:rounded-lg">
-              <h1 className="text-4xl font-bold text-gray-800 tracking-tight">
+            <div className="p-6 bg-customDark sm:rounded-lg">
+              <h1 className="text-4xl font-bold text-white tracking-tight">
                 Get in Touch
               </h1>
-              <p className="text-lg font-medium text-gray-600 mt-2">
+              <p className="text-lg font-medium text-white mt-2">
                 Fill in the form to start a conversation
               </p>
 
-              {/* Address */}
-              <div className="flex items-center mt-8 text-gray-600">
-                <svg
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="1.5"
-                  viewBox="0 0 24 24"
-                  className="w-8 h-8 text-gray-500">
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="1.5"
-                    d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
-                  />
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="1.5"
-                    d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
-                  />
-                </svg>
-                <div className="ml-4 text-md font-semibold">
-                  Acme Inc, Street, State, Postal Code
-                </div>
-              </div>
-
               {/* Phone Number */}
-              <div className="flex items-center mt-4 text-gray-600">
+              <div className="flex items-center mt-4 text-white">
                 <svg
                   fill="none"
                   stroke="currentColor"
@@ -52,7 +24,7 @@ export default function Contact() {
                   strokeLinejoin="round"
                   strokeWidth="1.5"
                   viewBox="0 0 24 24"
-                  className="w-8 h-8 text-gray-500">
+                  className="w-8 h-8 text-white">
                   <path
                     strokeLinecap="round"
                     strokeLinejoin="round"
@@ -60,11 +32,11 @@ export default function Contact() {
                     d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
                   />
                 </svg>
-                <div className="ml-4 text-md font-semibold">+44 1234567890</div>
+                <div className="ml-4 text-md font-semibold">+91 7233091999</div>
               </div>
 
               {/* Email */}
-              <div className="flex items-center mt-2 text-gray-600">
+              <div className="flex items-center mt-2 text-white">
                 <svg
                   fill="none"
                   stroke="currentColor"
@@ -80,7 +52,13 @@ export default function Contact() {
                     d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
                   />
                 </svg>
-                <div className="ml-4 text-md font-semibold">info@acme.org</div>
+                <a
+                  href="https://mail.google.com/mail/?view=cm&fs=1&to=mimohshukla0001@gmail.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="ml-4 text-md tracking-wide font-semibold w-40">
+                  mimohshukla0001@gmail.com
+                </a>
               </div>
             </div>
 
