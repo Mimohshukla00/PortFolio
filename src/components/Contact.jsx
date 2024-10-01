da841b90cfe60d1bd678b5050c02eadc8a917ccd
Mimohshukla00 <mimohshukla0001@gmail.com>
2024-10-02 02:58:41 +0530

    contact components

---
+++ src/components/Contact.jsx
@@ -0,0 +1,24 @@
+<div className="flex items-center mt-2 text-black">
+  <svg
+    fill="none"
+    stroke="currentColor"
+    strokeLinecap="round"
+    strokeLinejoin="round"
+    strokeWidth="1.5"
+    viewBox="0 0 24 24"
+    className="w-8 h-8 text-black">
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="1.5"
+      d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+    />
+  </svg>
+  <a
+    href="https://mail.google.com/mail/?view=cm&fs=1&to=mimohshukla0001@gmail.com"
+    target="_blank"
+    rel="noopener noreferrer"
+    className="ml-4 text-md tracking-wide font-semibold w-40">
+    mimohshukla0001@gmail.com
+  </a>
+</div>;
